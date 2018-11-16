f1c8da4091d8b228926e85ba0a053eacf170a53e
dan-lyn <daniel_lynge@hotmail.com>
2018-11-16 09:17:47 +0100

    changed header into functional component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Header extends Component {
-    render() {
-        return (
-            <section className="hero is-info is-small">
-                <div className="hero-body">
-                    <div className="container">
-                        <h1 className="title">reactivatoradmin</h1>
-                    </div>
-                </div>
-            </section>
-        );
-    }
-}
+const Header = (props: any) => (
+    <section className="hero is-info is-small">
+        <div className="hero-body">
+            <div className="container">
+                <h1 className="title">reactivatoradmin</h1>
+            </div>
+        </div>
+    </section>
+);
 
 export default Header;
