17a8104135f15132b4c41d10826b32f749eecfdf
dan-lyn <daniel_lynge@hotmail.com>
2018-11-16 08:56:09 +0100

    fixed image fallback

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -42,7 +42,7 @@ class Item extends Component<ItemProps, {}> {
                     <span>{item.name}</span>
                 </div>
                 <div className="column is-4 has-text-left">
-                    <img src={item.image} height="80" />
+                    <img src={item.image ? item.image : '/images/default.jpg'} height="80" />
                 </div>
                 <div className="column is-3 has-text-right item--action-buttons">
                     <button className="button is-info" onClick={this.showPopupInfo}>Info</button>
