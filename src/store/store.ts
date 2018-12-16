f1b3b94a80b54758c25f516668e887f024703428
dan-lyn <daniel_lynge@hotmail.com>
2018-12-16 20:16:36 +0100

    fixed devtools fallback function

--- src/store/store.ts
+++ src/store/store.ts
@@ -32,7 +32,7 @@ const initialState: State = {
 const rootReducer = combineReducers(initialState);
 
 const devTools = (window as any).__REDUX_DEVTOOLS_EXTENSION__ ?
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__() : () => {};
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__() : (a: any) => a;
 
 const sagaMiddleware = createSagaMiddleware();
 
