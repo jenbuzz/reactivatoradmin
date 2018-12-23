d4cf2feb9558a3daa4a4b109855de79087558e83
dan-lyn <daniel_lynge@hotmail.com>
2018-12-23 20:36:07 +0100

    moved app test to deprecated - keeping it as reference

---
+++ src/deprecated/App.test.tsx
@@ -0,0 +1,17 @@
+/**
+ * DEPRECATED
+ * 
+ * This test is kept as reference for testing without enzyme.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './../App';
+
+describe('<App />', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+});
