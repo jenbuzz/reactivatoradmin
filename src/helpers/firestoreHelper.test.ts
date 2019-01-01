7b3a7f8ee8abcdf980950bd926960a02698e1522
dan-lyn <daniel_lynge@hotmail.com>
2019-01-01 19:20:02 +0100

    fixed firestore helper test

--- src/helpers/firestoreHelper.test.ts
+++ src/helpers/firestoreHelper.test.ts
@@ -1,30 +1,41 @@
-import { getCollectionName, getCollectionCount } from './firestoreHelper';
+import FirestoreHelper from './firestoreHelper';
 
 describe('firestoreHelper -> getCollectionName', () => {
+    const firestoreHelper = new FirestoreHelper({});
+
     test('should return collection name', () => {
         const collectionName = 'test';
 
         process.env.REACT_APP_FIREBASE_COLLECTION = collectionName;
 
-        expect(getCollectionName()).toEqual(collectionName);
+        expect(firestoreHelper.getCollectionName()).toEqual(collectionName);
     });
 
     test('should throw an error if collection name is not set', () => {
         process.env.REACT_APP_FIREBASE_COLLECTION = '';
 
         expect(() => {
-            getCollectionName();
+            firestoreHelper.getCollectionName();
         }).toThrow();
     });
 });
 
 describe('firestoreHelper -> getCollectionCount', () => {
+    const getMock = jest.fn(() => {
+        return new Promise((resolve, reject) => {
+            resolve({size: 2});
+        });
+    });
+    const collectionMock = jest.fn((collectionName: string) => {return {get: getMock}});
+
+    const firestoreHelper = new FirestoreHelper({collection: collectionMock});
+
     test('should return collection count', () => {
         process.env.REACT_APP_FIREBASE_COLLECTION = 'test';
 
         expect.assertions(1);
-        return getCollectionCount().then((count: number) => {
-            expect(count).toEqual(0);
+        return firestoreHelper.getCollectionCount().then((count: number) => {
+            expect(count).toEqual(2);
         });
     });
 
@@ -32,7 +43,7 @@ describe('firestoreHelper -> getCollectionCount', () => {
         process.env.REACT_APP_FIREBASE_COLLECTION = '';
 
         expect(() => {
-            getCollectionCount();
+            firestoreHelper.getCollectionCount();
         }).toThrow();
     });
 });
--- src/store/store.ts
+++ src/store/store.ts
@@ -6,11 +6,11 @@ import 'firebase/firestore';
 import effects from './effects';
 
 const firebaseApp = firebase.initializeApp({
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || 'null',
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY || 'null',
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || 'null',
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL || 'null',
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || 'null',
 });
 
 const firestore = firebase.firestore(firebaseApp);
