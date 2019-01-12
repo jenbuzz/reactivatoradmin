b33383b516abdead6a9042a4594b4114fa8bbd2c
dan-lyn <daniel_lynge@hotmail.com>
2019-01-12 10:54:49 +0100

    added test for add item

--- src/components/AddItem/AddItem.test.tsx
+++ src/components/AddItem/AddItem.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { ItemContent } from './../Item';
 import AddItem from './AddItem';
 
@@ -8,5 +8,48 @@ describe('<AddItem />', () => {
         const addItem = (item: ItemContent) => {};
 
         shallow(<AddItem addItem={addItem} />);
+    });
+
+    it('should submit add-item form', () => {
+        const addItem = jest.fn();
+
+        const wrapper = mount(<AddItem addItem={addItem} />);
+        wrapper.find('button').simulate('submit');
+
+        const state = wrapper.state() as any;
+
+        expect(addItem).toBeCalled();
+        expect(state.name).toEqual('');
+        expect(state.image).toEqual('');
+    });
+
+    it('should update name in state on input change', () => {
+        const addItem = (item: ItemContent) => {};
+
+        const wrapper = mount(<AddItem addItem={addItem} />);
+
+        const input = wrapper.find('input').at(0);
+        const inputInstance = input.instance() as any;
+        inputInstance.value = 'abc';
+        input.simulate('change');
+
+        const state = wrapper.state() as any;
+
+        expect(state.name).toEqual('abc');
+    });
+
+    it('should update image in state on input change', () => {
+        const addItem = (item: ItemContent) => {};
+
+        const wrapper = mount(<AddItem addItem={addItem} />);
+
+        const input = wrapper.find('input').at(1);
+        const inputInstance = input.instance() as any;
+        inputInstance.value = 'abc';
+        input.simulate('change');
+
+        const state = wrapper.state() as any;
+
+        expect(state.image).toEqual('abc');
     });
 });
