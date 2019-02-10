8e5a2eb2f7d7bcae311cde5129394c417ccd083d
dan-lyn <daniel_lynge@hotmail.com>
2019-02-10 10:13:13 +0100

    setup font-awesome packages

--- src/index.tsx
+++ src/index.tsx
@@ -11,8 +11,9 @@ import * as serviceWorker from './serviceWorker';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faHome, faLightbulb } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faHome, faLightbulb);
+library.add(faHome, faLightbulb, faGithub);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
