29e6ef246ae1f0faac8bfba0b86e56b7d22351fc
dan-lyn <daniel_lynge@hotmail.com>
2019-02-10 10:02:35 +0100

    setup font-awesome packages

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,11 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHome, faLightbulb } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faHome, faLightbulb);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
